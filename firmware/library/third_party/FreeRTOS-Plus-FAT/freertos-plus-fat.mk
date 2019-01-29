c4fc4c52f2862326ec13a2d52433310a98f3aa00
Sameer Azer <azer.sameer@gmail.com>
2019-01-28 20:21:00 -0800

    Added FreeRTOS-Plus-FAT folder to third-party folder

---
+++ firmware/library/third_party/FreeRTOS-Plus-FAT/freertos-plus-fat.mk
@@ -0,0 +1,31 @@
+INCLUDES +=
+
+SYSTEM_INCLUDES +=$(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/include
+
+SOURCES += $(LIB_DIR)/third_party/FreRTOS-Plus-FAT/ff_crc.c
+SOURCES += $(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/ff_dev_support.c
+SOURCES += $(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/ff_dir.c
+SOURCES += $(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/ff_error.c
+SOURCES += $(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/ff_fat.c
+SOURCES += $(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/ff_file.c
+SOURCES += $(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/ff_format.c
+SOURCES += $(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/ff_ioman.c
+SOURCES += $(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/ff_locking.c
+SOURCES += $(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/ff_memory.c
+SOURCES += $(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/ff_stdio.c
+SOURCES += $(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/ff_string.c
+SOURCES += $(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/ff_sys.c
+SOURCES += $(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/ff_time.c
+
+
+
+
+
+
+
+
+
+SYSTEM_INCLUDES +=$(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/portable/common
+SOURCES += $(LIB_DIR)/third_party/FreeRTOS-Plus-FAT/common/ff_ramdisk.c
+
+TESTS +=
